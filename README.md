43ac7b0b3c85a7a133d69fe3a07c143e30ad97ec
farid_linein <faridlinein@users.noreply.github.com>
2020-03-23 14:52:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DOCKERIZE_DJANGO
+ 
---
+++ web/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# dos2unix.exe your-file.sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py collectstatic --no-input --clear
+
+exec "$@"
